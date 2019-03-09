51aa10eea3e2198c3e5755831b5790f11f671181
Kyrel Jerome <kyrel2007@gmail.com>
2019-03-09 00:27:06 -0500

    adsad

--- neo-deerhunt-master/client/grid_player.py
+++ neo-deerhunt-master/client/grid_player.py
@@ -9,9 +9,10 @@ class GridPlayer:
         melees = your_units.get_all_unit_of_type('melee')
         moves = []
         for worker in workers:
-            if worker != None:  
+            if worker != None:
+                print("Worker:" + str(worker.id))
                 if worker.can_mine(game_map):
-                    print("Worker:" + str(worker.id))
+                    print("Mining:" + str(worker.id))
                     moves.append(worker.mine())
                 else:
                     # move worker towards resource
@@ -26,22 +27,26 @@ class GridPlayer:
                             print("Move" + str(move.to_tuple()))
                             moves.append(move)
         for melee in melees:
-            if melee != None:  
-                if len(melee.can_attack(enemy_units)) >= 1:
-                    print("melee:" + str(melee.id))
-                    moves.append(melee.mine())
+            if melee != None:
+                targets = melee.can_attack(enemy_units)
+                print("melee: " + str(melee.id))
+                if len(targets) >= 1:
+                    print("Mining: " + str(melee.id))
+                    moves.append(melee.attack(melee.direction_to(targets[0])))
                 else:
                     # move worker towards resource
-                    resources = game_map.closest_resources(melee)
-                    print("Turn:" + str(100 - turns_left))
-                    print("Resources: " + str(resources))
-                    melee_path = game_map.bfs(melee.position(),resources)
-                    if melee_path != None and len(melee_path) > 1:
-                        print("Goal: " + str(melee_path[1]))
-                        if is_open(game_map, melee_path[1], your_units, enemy_units):
-                            move = melee.move_towards(melee_path[1])
-                            print("Move" + str(move.to_tuple()))
-                            moves.append(move)
+                    enemies = melee.nearby_enemies_by_distance(enemy_units)
+                    if len(enemies) > 0:
+                        enemy = enemy_units.get_unit_by_id(enemies[0][0])
+                        print("Turn:" + str(100 - turns_left))
+                        print("Resources: " + str(resources))
+                        melee_path = game_map.bfs(melee.position(),enemy)
+                        if melee_path != None and len(melee_path) > 2:
+                            print("Goal: " + str(melee_path[1]))
+                            if is_open(game_map, melee_path[1], your_units, enemy_units):
+                                move = melee.move_towards(melee_path[1])
+                                print("Move" + str(move.to_tuple()))
+                                moves.append(move)
         return moves
 
 def is_open(game_map, position, your_units, enemy_units):
@@ -52,7 +57,6 @@ def is_open(game_map, position, your_units, enemy_units):
         if position == enemy_units.get_unit(unit).position():
             return False
     return not game_map.is_wall(position[0], position[1])
-    pass
 
 '''
 for melee in melees:
